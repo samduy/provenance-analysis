a9ef9b9c93d3b5b6cff8bb5bab185c1ad86d7911
Sam Duy <khuong@eurecom.fr>
2017-04-26 07:32:54 +0200

    Extracted information from latest published release.
     Information extracted: name, tag name, created date, published date.
    
     In case of getting only TAGS (many projects don't have PUBLISHED
     RELEASES) we need some more processes in order to get the latest
     information.

--- github_latest.py
+++ github_latest.py
@@ -3,6 +3,7 @@
 import sys
 import json
 import requests
+from pprint import pprint
 
 GITHUB_URL = 'https://api.github.com'
 SEARCH_URL = GITHUB_URL+'/repos'
@@ -17,16 +18,22 @@ else:
   user = str(sys.argv[1])
   repo = str(sys.argv[2])
 
+# Formulate the request url
 url = SEARCH_URL+"/"+user+"/"+repo
 urltag = url + "/tags"
-urlrel = url + "/releases"
+urlrel = url + "/releases/latest"
+
+body = None
+rtype = None  #Type: Published release OR tag
 
 # Request
 r = requests.get(urlrel)
 if ((r.ok) and (len(r.text) > 2)): #work-around
+  rtype = "[RELEASE]"
   body = r.text
-  print body
+  #print body
 else: 
+  rtype = "[TAG]"
   r = requests.get(urltag)
   if (r.ok): body = r.text
   else: body = None
@@ -36,6 +43,10 @@ else:
 if (body):
   # Extract JSON data
   data = json.loads(body)
-  print data
+
+  if (rtype != "[RELEASE]"):
+    print pprint(data)
+  else:
+    print rtype + ",name:"+ data[u'name'] + ",tag:" + data[u'tag_name'] + ",created_at:" + data[u'created_at'] + ",published_at:" + data[u'published_at']
 else: 
   print "No data."
