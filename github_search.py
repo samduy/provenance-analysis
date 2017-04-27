3c60027a4a85b3c3c09f3a1210f66946c719729b
Sam Duy <khuong@eurecom.fr>
2017-04-27 05:50:42 +0200

    Added TOKEN for search. Added support for searching by file path.

--- github_search.py
+++ github_search.py
@@ -8,6 +8,8 @@ import requests
 
 GITHUB_URL = 'https://api.github.com'
 SEARCH_URL = GITHUB_URL+'/search'
+TOKEN = 'token'
+TOKEN_FILE = "./token"
 
 #TODO: Consider using PyGithub if neccesary.
 
@@ -22,6 +24,16 @@ else:
 
 url = SEARCH_URL+"/"+query
 
+# Check token
+try:
+  f = open(TOKEN_FILE, "r")
+except:
+  print "Unable to get token key. Please get the Github API token key for your account and save it in: " + TOKEN_FILE
+  print '$ curl -i -u <YourUserName> -d \'{"scopes": ["repo", "user"], "note": "Getting-started"}\' https://api.github.com/authorizations'
+  sys.exit()
+token_key = f.read().strip()
+TOKEN = TOKEN + " " + token_key
+
 # Request
 
 # (CURL way)
@@ -35,11 +47,21 @@ url = SEARCH_URL+"/"+query
 # body = buffer.getvalue()
 
 # (Classic way)
-r = requests.get(url)
+r = requests.get(url, headers={'Authorization':TOKEN})
 if (r.ok): body = r.text
 
 #print body
 
 # Extract JSON data
 data = json.loads(body)
-print data
+if (category != "code"):
+  print data
+else:
+  for i in data["items"]:
+    repo = i["repository"]
+    repo_name = repo["name"]
+    repo_full_name = repo["full_name"]
+    owner_name = repo["owner"]["login"]
+    html_url = repo["html_url"]
+    print  html_url 
+
