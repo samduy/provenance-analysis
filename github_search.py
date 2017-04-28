c7a55269924057e41b2f3a016f2af44fe351b794
Sam Duy <khuong@eurecom.fr>
2017-04-28 12:39:22 +0200

    Refine source code.

--- github_search.py
+++ github_search.py
@@ -2,9 +2,7 @@
 
 import sys
 import json
-#import pycurl
 import requests
-#from StringIO import StringIO
 
 GITHUB_URL = 'https://api.github.com'
 SEARCH_URL = GITHUB_URL+'/search'
@@ -13,6 +11,29 @@ TOKEN_FILE = "./token"
 
 #TODO: Consider using PyGithub if neccesary.
 
+# Request to GitHub
+def github_request(url):
+  data = None
+  error = None
+  r = requests.get(url, headers={'Authorization':TOKEN})
+  if ((r.ok) and (len(r.text) > 2)): #work-around
+    data = json.loads(r.text)
+  else:
+    error = json.loads(r.text)
+  return data, error
+
+# Load the GitHub token key from file
+def load_token_key(filename):
+  # Check token
+  try:
+    f = open(filename, "r")
+  except:
+    print "Unable to get token key. Please get the Github API token key for your account and save it in: " + filename
+    print 'Reference: https://api.github.com/authorizations'
+    sys.exit()
+  return 'token '+f.read().strip()
+
+## MAIN
 # Check arguments
 if len(sys.argv) < 2:
   print "Usage: %s <category:repositories|users|commits|code|issues> <keyword>" %sys.argv[0]
@@ -24,39 +45,14 @@ else:
 
 url = SEARCH_URL+"/"+query
 
-# Check token
-try:
-  f = open(TOKEN_FILE, "r")
-except:
-  print "Unable to get token key. Please get the Github API token key for your account and save it in: " + TOKEN_FILE
-  print '$ curl -i -u <YourUserName> -d \'{"scopes": ["repo", "user"], "note": "Getting-started"}\' https://api.github.com/authorizations'
-  sys.exit()
-token_key = f.read().strip()
-TOKEN = TOKEN + " " + token_key
+# Token key
+TOKEN = load_token_key(TOKEN_FILE)
 
 # Request
-
-# (CURL way)
-# buffer = StringIO()
-# c = pycurl.Curl()
-# c.setopt(c.URL, url)
-# c.setopt(c.WRITEDATA, buffer)
-# c.perform()
-# c.close()
-# 
-# body = buffer.getvalue()
-
-# (Classic way)
-r = requests.get(url, headers={'Authorization':TOKEN})
-if (r.ok): body = r.text
-
-#print body
-
-# Extract JSON data
-data = json.loads(body)
+data, error = github_request(url)
 if (category != "code"):
   print data
-else:
+elif (data):
   for i in data["items"]:
     repo = i["repository"]
     repo_name = repo["name"]
@@ -64,4 +60,5 @@ else:
     owner_name = repo["owner"]["login"]
     html_url = repo["html_url"]
     print  html_url 
-
+else:
+  print "Error: " + error["message"]
