6d3bc2cad31fad991b4d044baf5adb79b868ff10
Sam Duy <khuong@eurecom.fr>
2017-04-30 19:38:41 +0200

    Added file for getting information from Internet.
    This script will get a number of files that have longest file paths
    inside the provided program dir, from the interesting list.
    
    Then, use GitHub API to search for those filepaths to find their
    public repos on internet
    
    After that, it filters the results: get only the repo that appear in
    all returned results. (Since each search with individual file path
    returns more than one possible repo. The one we are looking for is
    the one that exist in all the returned results.)

---
+++ internet_info.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Check argument
+if [ ! $# -eq 2 ]; then
+	echo "Usage $0 <dir> <files_list>"
+	exit 1
+fi
+
+# Check file
+if [ ! -f $2 ]; then
+	echo "Invalid file: $2"
+	exit 1
+fi
+
+# Process
+NUMBER_OF_FILES=3 # Change this for better determination.
+
+#Get a number of files that have longest file paths inside the program dir
+longest_filepaths=$(grep $1 $2 | sed -r -n 's,('$1')/(.*),\2,p' | awk '{print length, $0}' | sort -nr | head -$NUMBER_OF_FILES | awk '{print $2}')
+#echo $longest_filepaths
+
+#Use GitHub API to search for those filepaths to find their public repo on internet
+for filepath in $longest_filepaths; do \
+  full_filepath=$1/$filepath;
+  #echo $full_filepath;
+  internet_repos=$(python ./github_filesearch.py $filepath | sort | uniq -u)
+  for line in $internet_repos; do \
+    echo $line; \
+    done; \
+
+# Filter the result: get only the repo that appear in all returned results
+# (Since each search with individual file path returns more than one possible repo. 
+# The one we are looking for is the one that exist in all the returned results.)
+done | sort | uniq -c | gawk '$1=='$NUMBER_OF_FILES'{print $2}'
