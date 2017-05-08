7e56c31f581336e2ec70670e2bfd85e43d2432cb
Sam Duy <khuong@eurecom.fr>
2017-05-08 08:26:12 +0200

    For each program dir, get latest info from ALL available repos on GitHub.

--- internet_info.sh
+++ internet_info.sh
@@ -12,14 +12,18 @@ if [ ! -f $2 ]; then
 	exit 1
 fi
 
+dir=$1
+file_list=$2
+
 # Process
 NUMBER_OF_FILES=3 # Change this for better determination.
 
 #Get a number of files that have longest file paths inside the program dir
-longest_filepaths=$(grep $1 $2 | sed -r -n 's,('$1')/(.*),\2,p' | awk '{print length, $0}' | sort -nr | head -$NUMBER_OF_FILES | awk '{print $2}')
+longest_filepaths=$(grep $dir $file_list | sed -r -n 's,('$1')/(.*),\2,p' | awk '{print length, $0}' | sort -nr | head -$NUMBER_OF_FILES | awk '{print $2}')
 #echo $longest_filepaths
 
 #Use GitHub API to search for those filepaths to find their public repo on internet
+userrepos=$(
 for filepath in $longest_filepaths; do \
   full_filepath=$1/$filepath;
   #echo $full_filepath;
@@ -32,3 +36,8 @@ for filepath in $longest_filepaths; do \
 # (Since each search with individual file path returns more than one possible repo. 
 # The one we are looking for is the one that exist in all the returned results.)
 done | sort | uniq -c | gawk '$1=='$NUMBER_OF_FILES'{print $2}'
+)
+
+for userrepo in $userrepos; do \
+  echo $dir,$(./github_latest.py $userrepo); 
+done
